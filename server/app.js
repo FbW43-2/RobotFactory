c2a2310372c286462931c9fb4e85559eb699b5a3
QCzok <jan.czok@outlook.de>
2021-04-22 15:24:25 +0200

    template

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+const { setCors } = require("./middleware/security");
 
 const robotsRouter = require('./routes/robots');
 
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// omit cors errors
-app.use(cors());
+app.use(setCors);
 
 app.use('/', robotsRouter);
 
